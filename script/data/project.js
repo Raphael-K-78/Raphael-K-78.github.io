31f0bd022925a3dcaf38ff7b2530c024c88cc607
Raphaël Kondratiuk <rkondratiuk@eventtech-services.com>
2026-06-17 16:30:06 +0200

    indev_detail_project

--- script/data/project.js
+++ script/data/project.js
@@ -35,8 +35,6 @@ fetch('data/project.json')
                   popovertargetaction="hide">×</button>
 
           <h2>${project.name}</h2>
-          <md>${project.description_longue}</md>
-
           <div class="slider" data-slider>
             <div class="slides">
               ${slidesHTML}
@@ -45,6 +43,7 @@ fetch('data/project.json')
             <button class="nav prev" data-prev>&lt;</button>
             <button class="nav next" data-next>&gt;</button>
           </div>
+          <md>${project.description_longue}</md>
         </div>
       `;
 
